d2f94b243e7d47449253cc586d86bcbe32078666
riyaagar <agar.riya62@gmail.com>
2020-01-20 20:15:08 -0500

    Initial Commit

---
+++ tipi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipi
+//
+//  Created by Riya Agarwal on 1/20/20.
+//  Copyright © 2020 RAgarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
